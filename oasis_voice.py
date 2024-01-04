d290532881d3dd573176c42bb35cedc29f5c437f
singhisha40 <141261626+singhisha40@users.noreply.github.com>
2024-01-04 15:44:13 +0530

    Add files via upload

---
+++ oasis_voice.py
@@ -0,0 +1,80 @@
+import pyttsx3 #pip install pyttsx3
+import speech_recognition as sr #pip install speechRecognition
+import datetime
+import wikipedia #pip install wikipedia
+import webbrowser
+import os
+import smtplib
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning Isha")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon Isha")   
+
+    else:
+        speak("Good Evening Isha")  
+
+    speak("I am your voice assistant. Please tell me how may I help you")       
+
+def takeCommand():
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        # print(e)    
+        print("Can you please repeat...")  
+        return "None"
+    return query
+
+
+if __name__ == "__main__":
+    wishMe()
+    while True:
+
+        query = takeCommand().lower()
+
+        
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+      
+        elif 'open github' in query:
+            webbrowser.open("github.com")
+            
+
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
+            speak(f"Sir, the time is {strTime}")
+
